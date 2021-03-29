95745a565bf5b1f9ef3f62715a4836f967ea9afd
Bartosz Świtalski <b.switalski@stud.elka.pw.edu.pl>
2021-03-29 11:24:40 +0200

    dataset analysis

---
+++ purchase-prediction/main.py
@@ -0,0 +1,15 @@
+"""
+    Name:
+    Purpose:
+
+    @author Bartosz Świtalski, Piotr Frątczak
+
+    Warsaw University of Technology
+    Faculty of Electronics and Information Technology
+"""
+from preprocess.json_read import *
+
+if __name__ == '__main__':
+    # products_check()
+    # users_check()
+    sessions_check()
